ddb81a156c3d2289a9a4ff0c956395daf275077d
Abhishek Upperwal <a.upperwal@gmail.com>
2018-04-10 00:22:14 +0530

    Updated Travis

--- ci/travis/install_mpi.sh
+++ ci/travis/install_mpi.sh
@@ -24,6 +24,7 @@ else
 	echo "Configuring and building MPICH"
 	cd mpich-3.2.1
 	./configure --prefix=$TRAVIS_BUILD_DIR/mpich CC=$C_COMPILER CXX=$CXX_COMPILER &> mpich.configure
+	cat mpich.configure
 	make -j4 &> mpich.make
 	cat mpich.make
 	make install &> mpich.install
