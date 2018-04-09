33510d0e53936ed24116ed5854e957821e259e9a
Abhishek Upperwal <upperwal@users.noreply.github.com>
2018-04-10 02:27:44 +0530

    Added Travis
    * Added Travis CI config
    
    * Added Build status to README
    
    * Updated Travis

---
+++ ci/travis/install_mpi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    cd mpich
+    if [ -f "bin/mpirun" ]; then
+	echo "Using cached MPICH"
+    else
+	echo "Installing MPICH with homebrew"
+        brew install mpich
+	ln -s /usr/local/bin bin
+	ln -s /usr/local/lib lib
+	ln -s /usr/local/include include
+    fi
+else
+    if [ -f "mpich/bin/mpirun" ] && [ -f "mpich-3.2.1/config.log" ]; then
+	echo "Using cached MPICH"
+	echo "Configuring MPICH"
+	cd mpich-3.2.1
+	./configure --prefix=$TRAVIS_BUILD_DIR/mpich &> mpich.configure
+	cat mpich.configure
+	cd ..
+    else
+	echo "Downloading MPICH Source"
+	wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
+	tar zxf mpich-3.2.1.tar.gz
+	echo "Configuring and building MPICH"
+	cd mpich-3.2.1
+	./configure --prefix=$TRAVIS_BUILD_DIR/mpich --disable-fortran &> mpich.configure
+	#cat mpich.configure
+	make -j4 &> mpich.make
+	#cat mpich.make
+	make install &> mpich.install
+	#locate mpicc
+	cd ..
+    fi
+    #test -n $CC && unset CC
+    #test -n $CXX && unset CXX
+fi
