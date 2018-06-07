7a3fcc35ea30f48e38657e38f54f132fc55fa656
Abhishek Upperwal <a.upperwal@gmail.com>
2018-06-07 19:19:06 +0530

    added colors

--- src/misc/log.c
+++ src/misc/log.c
@@ -1,12 +1,21 @@
 #include "src/misc/log.h"
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
 extern Node node;
 
 va_list args;
 
 void _log_e(const char *module, const int line, const char *message, ...) {
 	char buffer[200];
-	sprintf(buffer, "ERROR: [Rank: %d] | Message: %s | Module: %s | Line: %d\n", node.static_rank, message, module, line);
+	sprintf(buffer, "%sERROR:%s [Rank: %d] | Message: %s | Module: %s | Line: %d\n", KRED, KNRM, node.static_rank, message, module, line);
 	
 	va_start(args, buffer);
 	vfprintf(stdout, buffer, args);
@@ -15,7 +24,7 @@ void _log_e(const char *module, const int line, const char *message, ...) {
 
 void _log_i(const char *module, const int line, const char *message, ...) {
 	char buffer[200];
-	sprintf(buffer, "INFO: [Rank: %d] | Message: %s | Module: %s | Line: %d\n", node.static_rank, message, module, line);
+	sprintf(buffer, "%sINFO:%s [Rank: %d] | Message: %s | Module: %s | Line: %d\n", KGRN, KNRM, node.static_rank, message, module, line);
 
 	va_start(args, buffer);
 	vfprintf(stdout, buffer, args);
