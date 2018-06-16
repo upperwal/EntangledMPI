b0b6902b7c8127cc1cad1c5402f3905b5b44e55d
Abhishek Upperwal <a.upperwal@gmail.com>
2018-06-16 22:14:07 +0530

    EntangledMPI stores info about stack address to disk

--- src/misc/file.c
+++ src/misc/file.c
@@ -1,9 +1,18 @@
 #include "file.h"
 
-int is_file_modified(char *file_name, time_t *last_update, enum CkptBackup *ckpt_backup) {
+extern Node node;
+extern address stackHigherAddress;
+
+typedef struct {
+	int replication_idx;
+	int rank;
+	address stack_address;
+} rep_and_stack_info;
+
+int is_file_modified(char *file_name, time_t *last_update, enum CkptRestore *ckpt_restore) {
 	struct stat file_attr;
 
-	if(*ckpt_backup == BACKUP_YES) {
+	if(*ckpt_restore == RESTORE_YES) {
 		return 1;
 	}
 
@@ -20,9 +29,24 @@ int is_file_modified(char *file_name, time_t *last_update, enum CkptBackup *ckpt
 	}
 }
 
-void set_last_update(char *file_name, time_t *last_update) {
+void set_last_file_update(char *file_name, time_t *last_update) {
 	struct stat file_attr;
 
 	stat(file_name, &file_attr);
 	*last_update = file_attr.st_mtime;
+}
+
+void save_rep_and_stack_info(int rep_idx) {
+	rep_and_stack_info rep_stack;
+	MPI_File rep_stack_info_file;
+	MPI_Offset offset = sizeof(rep_and_stack_info) * node.rank;
+
+	rep_stack.replication_idx = rep_idx;
+	rep_stack.rank = node.rank;
+	rep_stack.stack_address = stackHigherAddress;
+
+	// Writting network stats to a file.
+	PMPI_File_open(node.rep_mpi_comm_world, STACK_REP_INFO_FILE_NAME, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &rep_stack_info_file);
+	PMPI_File_write_at(rep_stack_info_file, offset, &rep_stack, sizeof(rep_and_stack_info), MPI_BYTE, MPI_STATUS_IGNORE);
+	PMPI_File_close(&rep_stack_info_file);
 }
--- src/misc/file.h
+++ src/misc/file.h
@@ -9,7 +9,9 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
-int is_file_modified(char *, time_t *, enum CkptBackup *);
-void set_last_update(char *, time_t *);
+int is_file_modified(char *, time_t *, enum CkptRestore *);
+void set_last_file_update(char *, time_t *);
+
+void save_rep_and_stack_info(int);
 
 #endif
